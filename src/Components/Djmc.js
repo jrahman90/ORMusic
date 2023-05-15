979a75f7731c9440a56f54ef4f5ff5080abd3dff
Jalilur Rahman <jrahman90.work@gmail.com>
2023-05-15 11:55:39 -0400

    pushig repo to github

--- src/Components/Djmc.js
+++ src/Components/Djmc.js
@@ -1,5 +1,18 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Djmc() {
-  return <div>DJ/MC</div>;
+  return (
+    <Container>
+      <Row className="mb-3 mt-3">Meet The Team</Row>
+      <Row>
+        <Col>
+          <img src="ormusiclogo.png" width={400} />
+        </Col>
+        <Col>2 of 2</Col>
+      </Row>
+    </Container>
+  );
 }
