a53d7baeb7c3358a8df4c9009fb23e1264608bae
Opu Rahman <Opu@jay-rahman.mynetworksettings.com>
2023-05-16 10:59:40 -0400

    added social media links in footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,12 +1,27 @@
 import React from "react";
 import "./Css/components.css";
+import { Container } from "react-bootstrap";
 
 export default function Footer() {
   return (
     <div>
-      <h3 align="center" vertical-align="bottom">
+      <Container className="mb-3" align='center'>
+        <a href="https://www.facebook.com/ormusicevents" target="_blank">
+        <img src="facebook.png"/>
+        </a>
+        <a href="https://www.instagram.com/ormusic.events/" target="_blank">
+        <img src="instagram.png"/>
+        </a>
+        <a href="https://www.youtube.com/channel/UCdtFAaHPQxzm-jKlPgKPngQ" target="_blank">
+        <img src="youtube.png"/>
+        </a>
+        <a href="https://www.tiktok.com/@officialopu" target="_blank">
+        <img src="tiktok.png"/>
+        </a>
+      <h3 align="center" vertical-align="bottom" style={{fontFamily: 'sans serif, arial or verdana'}}>
         © 2023 OR Music
       </h3>
+      </Container>
     </div>
   );
 }
