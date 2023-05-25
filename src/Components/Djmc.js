444e2c7a0f14a72c65c7734854dd393b3f85243b
Opu Rahman <Opu@jay-rahman.mynetworksettings.com>
2023-05-25 18:21:11 -0400

    djmc page update

--- src/Components/Djmc.js
+++ src/Components/Djmc.js
@@ -10,19 +10,21 @@ export default function Djmc() {
       <Row className="mb-3 mt-3">Meet The Team</Row>
       <Row>
         <Col className="mb-2">
-          <img src="dj1.jpg" width={400} />
+        <div className="loading">
+          <img alt="" src="dj1.jpg" width={400} />
+        </div>
         </Col>
         <Col className="mb-2">
-          <img src="dj600.png" width={400} />
+          <img alt="" src="dj600.png" width={400} />
         </Col>
         <Col className="mb-2">
-          <img src="djbooth.jpg" width={400} />
+          <img alt="" src="djbooth.jpg" width={400} />
         </Col>
         <Col className="mb-2">
-          <img src="djbooth2.jpg" width={400} />
+          <img alt="" src="djbooth2.jpg" width={400} />
         </Col>
         <Col className="mb-2">
-          <img src="djbooth3.jpg" width={400} />
+          <img alt="" src="djbooth3.jpg" width={400} />
         </Col>
       </Row>
     </Container>
--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,17 +6,17 @@ export default function Footer() {
   return (
     <div>
       <Container className="mb-3" align='center'>
-        <a href="https://www.facebook.com/ormusicevents" target="_blank">
-        <img src="facebook.png"/>
+        <a href="https://www.facebook.com/ormusicevents" target="_blank"  rel="noreferrer">
+        <img alt="" src="facebook.png"/>
         </a>
-        <a href="https://www.instagram.com/ormusic.events/" target="_blank">
-        <img src="instagram.png"/>
+        <a href="https://www.instagram.com/ormusic.events/" target="_blank" rel="noreferrer">
+        <img alt="" src="instagram.png"/>
         </a>
-        <a href="https://www.youtube.com/channel/UCdtFAaHPQxzm-jKlPgKPngQ" target="_blank">
-        <img src="youtube.png"/>
+        <a href="https://www.youtube.com/channel/UCdtFAaHPQxzm-jKlPgKPngQ" target="_blank" rel="noreferrer">
+        <img alt="" src="youtube.png"/>
         </a>
-        <a href="https://www.tiktok.com/@officialopu" target="_blank">
-        <img src="tiktok.png"/>
+        <a href="https://www.tiktok.com/@officialopu" target="_blank" rel="noreferrer">
+        <img alt="" src="tiktok.png"/>
         </a>
       <h3 align="center" vertical-align="bottom" style={{fontFamily: 'sans serif, arial or verdana'}}>
         © 2023 OR Music
