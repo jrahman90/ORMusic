3ac260b8f3dd8fc6c0718b066621a5aa78aff74c
Jalilur Rahman <jrahman90.work@gmail.com>
2023-05-25 19:03:24 -0400

    adjusted djmc images

--- src/Components/Djmc.js
+++ src/Components/Djmc.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import './Css/components.css'
+import "./Css/components.css";
 
 export default function Djmc() {
   return (
@@ -10,21 +10,19 @@ export default function Djmc() {
       <Row className="mb-3 mt-3">Meet The Team</Row>
       <Row>
         <Col className="mb-2">
-        <div className="loading">
-          <img alt="" src="dj1.jpg" width={400} />
-        </div>
+          <img alt="" src="dj1.jpg" width={350} />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="dj600.png" width={400} />
+          <img alt="" src="dj600.png" width={350} />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="djbooth.jpg" width={400} />
+          <img alt="" src="djbooth.jpg" width={350} />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="djbooth2.jpg" width={400} />
+          <img alt="" src="djbooth2.jpg" width={350} />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="djbooth3.jpg" width={400} />
+          <img alt="" src="djbooth3.jpg" width={350} />
         </Col>
       </Row>
     </Container>
