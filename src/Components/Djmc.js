7efa01a272abafd4e99addf0055f20896825180c
Opu Rahman <Opu@jay-rahman.mynetworksettings.com>
2023-05-26 02:14:41 -0400

    fixed djmc images

--- src/Components/Djmc.js
+++ src/Components/Djmc.js
@@ -2,29 +2,32 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Image from 'react-bootstrap/Image'
+import Spinner from 'react-bootstrap/Spinner';
 import "./Css/components.css";
 
 export default function Djmc() {
+  
   return (
     <Container className="djmc-container">
-      <Row className="mb-3 mt-3">Meet The Team</Row>
-      <Row>
+      <Row style={{justifyContent:'center'}} className="mb-3 mt-3" >Meet The Team</Row>
+       <Row >
         <Col className="mb-2">
-          <img alt="" src="dj1.jpg" width={350} />
+          <Image className="djmc-images" src="dj1.jpg" rounded fluid/>
+        </Col>
+        <Col className="mb-2" >
+          <Image className="djmc-images" src="dj600.png" rounded fluid/>
         </Col>
         <Col className="mb-2">
-          <img alt="" src="dj600.png" width={350} />
+          <Image className="djmc-images" src="djbooth.jpg" rounded fluid />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="djbooth.jpg" width={350} />
+          <Image className="djmc-images" src="djbooth2.jpg" rounded fluid />
         </Col>
         <Col className="mb-2">
-          <img alt="" src="djbooth2.jpg" width={350} />
-        </Col>
-        <Col className="mb-2">
-          <img alt="" src="djbooth3.jpg" width={350} />
-        </Col>
-      </Row>
+          <Image className="djmc-images" src="djbooth3.jpg" rounded fluid />
+        </Col> 
+       </Row> 
     </Container>
   );
 }
