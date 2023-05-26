8827768d2d085306b207ef5192d4854bc2dc85e2
Opu Rahman <Opu@jay-rahman.mynetworksettings.com>
2023-05-26 02:24:55 -0400

    confromed djmc page to bootstrap

--- src/Components/Djmc.js
+++ src/Components/Djmc.js
@@ -3,7 +3,6 @@ import { Container } from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from 'react-bootstrap/Image'
-import Spinner from 'react-bootstrap/Spinner';
 import "./Css/components.css";
 
 export default function Djmc() {
@@ -12,20 +11,20 @@ export default function Djmc() {
     <Container className="djmc-container">
       <Row style={{justifyContent:'center'}} className="mb-3 mt-3" >Meet The Team</Row>
        <Row >
-        <Col className="mb-2">
-          <Image className="djmc-images" src="dj1.jpg" rounded fluid/>
+        <Col className="mb-2" xs={6} md={4}>
+          <Image  src="dj1.jpg" rounded fluid/>
         </Col>
-        <Col className="mb-2" >
-          <Image className="djmc-images" src="dj600.png" rounded fluid/>
+        <Col className="mb-2"xs={6} md={4} >
+          <Image  src="dj600.png" rounded fluid/>
         </Col>
-        <Col className="mb-2">
-          <Image className="djmc-images" src="djbooth.jpg" rounded fluid />
+        <Col className="mb-2"xs={6} md={4}>
+          <Image  src="djbooth.jpg" rounded fluid />
         </Col>
-        <Col className="mb-2">
-          <Image className="djmc-images" src="djbooth2.jpg" rounded fluid />
+        <Col className="mb-2"xs={6} md={4}>
+          <Image  src="djbooth2.jpg" rounded fluid />
         </Col>
-        <Col className="mb-2">
-          <Image className="djmc-images" src="djbooth3.jpg" rounded fluid />
+        <Col className="mb-2"xs={6} md={4}>
+          <Image  src="djbooth3.jpg" rounded fluid />
         </Col> 
        </Row> 
     </Container>
