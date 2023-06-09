322644393dbb12e203be59c0a43ce5ed2ce81766
jalilur rahman <43665608+jrahman90@users.noreply.github.com>
2023-06-08 20:24:14 -0400

    infinite loop fix (#2)
    * added cart components
    
    * fixed infinite loop in user data fetch
    
    * cleanup
    
    ---------
    
    Co-authored-by: Opu Rahman <Opu@jay-rahman.mynetworksettings.com>

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState} from "react";
+import React, { useState, useEffect} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import "./Components/Css/components.css";
@@ -18,37 +18,52 @@ import { getAuth, onAuthStateChanged } from "@firebase/auth";
 import { getDoc, doc } from "@firebase/firestore";
 import firestore from "./api/firestore/firestore";
 
+import Cart from "./Components/User Components/Cart";
+import RentalsAdmin from "./Components/Admin/RentalsAdmin";
+import Rentals from "./Components/User Components/Rentals";
 
 function App() {
-  const [isAdmin, setIsAdmin] = useState(null)
   const [userData, setUserData] = useState(null)
+  const [cartItems, setCartItems] = useState([]);
   const auth = getAuth()
   const db = firestore
 
-  onAuthStateChanged(auth, async (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      const docRef = doc(db, "users", uid);
-      try {
-        const docSnap = await getDoc(docRef);
-        if(docSnap.exists()) {
-            setUserData(docSnap.data());
-            setIsAdmin(userData?.isAdmin)
-        } else {
-            console.log("Document does not exist")
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const uid = user.uid;
+        const docRef = doc(db, "users", uid);
+        try {
+          const docSnap = await getDoc(docRef);
+          if(docSnap.exists()) {
+              setUserData(docSnap.data());
+          } else {
+              console.log("Document does not exist")
+          }
+        } catch(error) {
+          console.log(error)
         }
-    
-    } catch(error) {
-        console.log(error)
-    }
-      // ...
+      } else {
+        console.log('User is not logged in.')
+      }
+    });
+    // eslint-disable-next-line
+  }, [])
+  
+  const addToCart = (item) => {
+    const existingItemIndex = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+
+    if (existingItemIndex !== -1) {
+      const updatedCartItems = [...cartItems];
+      updatedCartItems[existingItemIndex] = {
+        ...updatedCartItems[existingItemIndex],
+        quantity: updatedCartItems[existingItemIndex].quantity + 1,
+      };
+      setCartItems(updatedCartItems);
     } else {
-      // User is signed out
-      // ...
+      setCartItems([...cartItems, { ...item, quantity: 1 }]);
     }
-  });
+  };
 
   return (
     <div>
@@ -56,11 +71,15 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/contact" element={<ContactUs />} />
-        <Route exact path="/DJMC" element={isAdmin ? <DjmcAdmin /> : <Djmc />} />
+        <Route exact path="/DJMC" element={userData?.isAdmin ? <DjmcAdmin /> : <Djmc />} />
         <Route exact path="/Downloads" element={<Downloads />} />
-        <Route exact path="/MusicVideos" element={isAdmin?<MusicVideoAdmin/>:<MusicVideos />} />
+        <Route exact path="/MusicVideos" element={userData?.isAdmin?<MusicVideoAdmin/>:<MusicVideos />} />
         <Route exact path="/Music" element={<Music />} />
+        <Route exact path="/Cart" element={<Cart items={cartItems} setItems={setCartItems}/>}/>
+        {/* <Route exact path='/RentalItems' element={isLoggedIn?<RentalsAdmin/>:<Rentals addToCart={addToCart}/>}/> */}
+        <Route exact path='/RentalItems' element={userData?.isAdmin?<RentalsAdmin/>:<Rentals addToCart={addToCart} />}/>
         <Route path="/*" element={<PageNotFound/>}/>
+
       </Routes>
       <div>
         <div className="line"></div>
