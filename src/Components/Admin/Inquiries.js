a7b92aa82d6d3a54ec64930957400771e42a7842
Jalilur Rahman <jrahman90.work@gmail.com>
2024-02-16 18:29:03 -0500

    created inquiry page

--- src/Components/Admin/Inquiries.js
+++ src/Components/Admin/Inquiries.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Card, Col, Row } from "react-bootstrap";
 import { collection, query, orderBy, getDocs } from "firebase/firestore";
 import db from "../../api/firestore/firestore";
 import { Container } from "react-bootstrap";
@@ -30,12 +31,35 @@ export default function Inquiries() {
 
     fetchInquiries();
   }, []);
-  console.log(inquiries);
   return (
-    <Container>
-      {inquiries.map((inquiry) => {
-        console.log(inquiry);
-      })}
+    <Container style={{ marginTop: "1rem" }}>
+      <Row xs={1} md={2} lg={3} className="g-4" align="center">
+        {inquiries.map((inquiry, idx) => (
+          <Col key={idx}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{inquiry.status}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {inquiry.phoneNumber}
+                </Card.Subtitle>
+                <Card.Subtitle className="mb-2 text-muted">
+                  {inquiry.email}
+                </Card.Subtitle>
+                <Card.Text>{inquiry.eventDetails}</Card.Text>
+                <div className="line"></div>
+                {inquiry.items.map((item) => (
+                  <>
+                    <Card.Text>{`${item.name} - Q: ${item.quantity}`}</Card.Text>
+                    <Card.Text>{item.description}</Card.Text>
+                    <div className="line-dotted" />
+                  </>
+                ))}
+                <Card.Text></Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Container>
   );
 }
